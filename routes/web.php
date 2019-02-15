f76a4e112700fd7ecf3f56a4f1ad2e4fbcb29bbc
ArturHanusek <arthur@youritsolutions.ie>
2019-02-15 17:52:38 +0100

    Added spatie/laravel-google-calendar

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,17 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+use Spatie\GoogleCalendar\Event;
+use Spatie\GoogleCalendar\GoogleCalendar;
 
 Route::get('/', function () {
+
+    $events = Event::get();
+
+    $events[0]->startDate;
+    $events[0]->startDateTime;
+    $events[0]->endDate;
+    $events[0]->endDateTime;
+    return $events;
     return view('calendar');
 });
