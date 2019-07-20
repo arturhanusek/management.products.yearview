1ee48d4e24e0149b9622797d54b989d6ee2f52ee
Artur Hanusek <arthur@youritsolutions.ie>
2019-07-20 13:52:53 +0200

    change jquery to load trough https

--- resources/views/calendar.blade.php
+++ resources/views/calendar.blade.php
@@ -99,7 +99,7 @@
 
         </script>
 
-        <script src='http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js'></script>
+        <script src='https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js'></script>
         <script src='https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js'></script>
 
         <script  src="js/index.js"></script>
